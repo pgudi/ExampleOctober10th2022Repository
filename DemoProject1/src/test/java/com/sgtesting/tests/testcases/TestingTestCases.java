7221bf9c759a1a9de5a9de9a0471930f8e843d2a
pgudi <prabhakargudi@gmail.com>
2023-01-11 17:14:36 +0530

    TDD Process

---
+++ DemoProject1/src/test/java/com/sgtesting/tests/testcases/TestingTestCases.java
@@ -0,0 +1,18 @@
+package com.sgtesting.tests.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestingTestCases {
+	
+	@Test
+	public void testcase1()
+	{
+		System.out.println("It is First Testng Testcase...");
+	}
+
+	@Test
+	public void testcase2()
+	{
+		System.out.println("It is Second Testng Testcase...");
+	}
+}
